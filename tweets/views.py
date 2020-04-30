49377b102983ada3fe38e5799fdb11bf110b209b
Francisco <javiermartincuenca@gmail.com>
2020-04-30 21:56:26 +0200

    10 - Intro to URL routing

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def home_view(request,*args, **kwargs):
+    return HttpResponse("<h1>Hello world</h1>")
+
+def tweet_detail_view(request, tweet_id, *args, **kwargs):
+    return HttpResponse(f"<h1>Tweet {tweet_id}</h1>")
