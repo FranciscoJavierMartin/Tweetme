dd66a0574affcc38b0781a6789aa4ab6d589a5c7
Francisco <javiermartincuenca@gmail.com>
2020-06-11 22:01:11 +0200

    21 - Rapid implement of Bootstrap theme

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,4 @@
+import random
 from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render
 from .models import Tweet
@@ -10,7 +11,7 @@ def home_view(request, *args, **kwargs):
 
 def tweets_list_view(request, *args, **kwargs):
     qs = Tweet.objects.all()
-    tweets_list = [{'id': x.id, 'content': x.content} for x in qs]
+    tweets_list = [{'id': x.id, 'content': x.content, 'likes': random.randint(0, 100)} for x in qs]
     data = {
         'isUser': False,
         'response': tweets_list
