73d3ad8202c7a4359db2300c4e6dee2a9c29e23f
Francisco <javiermartincuenca@gmail.com>
2020-06-25 22:03:55 +0200

    30 - Append new tweet and reorder

--- tweets/models.py
+++ tweets/models.py
@@ -8,6 +8,9 @@ class Tweet(models.Model):
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='image/', blank=True, null=True)
 
+    class Meta:
+        ordering = ['-id']
+
     def serialize(self):
         return {
             'id': self.id,
