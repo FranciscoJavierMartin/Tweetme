af5b2d2be9f85cc96f008681f25522fc6ce9fdd4
Francisco <javiermartincuenca@gmail.com>
2020-07-27 23:20:43 +0200

    55 - Handling CORS and invalid HOST_HEADER in Django

--- tweetme-web/src/App.tsx
+++ tweetme-web/src/App.tsx
@@ -1,11 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
-  return (
-    <h1>Hello world</h1>
-  );
+function loadTweets(callback: any){
+  const xhr = new XMLHttpRequest();
+  const method = 'GET';
+  const url = 'http://localhost:8000/api/tweets';
+  const responseType = 'json';
+
+  xhr.responseType = responseType;
+  xhr.open(method, url);
+  xhr.onload = function () {
+      callback(xhr.response, xhr.status)
+  };
+  xhr.onerror = () => {
+    callback({'message': 'The request was an error'}, 400);
+  };
+  xhr.send();
 }
+
+const App = () => {
+  const [tweets, setTweets] = useState<any[]>([]);
+
+  useEffect(() => {
+    loadTweets((response: any[], status) => {
+      if(status === 200){
+        setTweets(response)
+      }
+    })
+  }, []);
+
+  return <div></div>;
+};
 
 export default App;
--- tweetme/settings.py
+++ tweetme/settings.py
@@ -43,12 +43,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'tweets',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -125,6 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_URLS_REGEX = r'^/api/.*$'
 
 DEFAULT_RENDERER_CLASSES = [
     'rest_framework.renderers.JSONRenderer',
