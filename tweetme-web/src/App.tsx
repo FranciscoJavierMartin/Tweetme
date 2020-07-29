710d37ebfc2161c2fa52b12063463c71e0577c65
Francisco <javiermartincuenca@gmail.com>
2020-07-29 19:43:30 +0200

    56 - Functional components in React

--- tweetme-web/src/App.tsx
+++ tweetme-web/src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 
-function loadTweets(callback: any){
+function loadTweets(callback: any) {
   const xhr = new XMLHttpRequest();
   const method = 'GET';
   const url = 'http://localhost:8000/api/tweets';
@@ -9,10 +9,10 @@ function loadTweets(callback: any){
   xhr.responseType = responseType;
   xhr.open(method, url);
   xhr.onload = function () {
-      callback(xhr.response, xhr.status)
+    callback(xhr.response, xhr.status);
   };
   xhr.onerror = () => {
-    callback({'message': 'The request was an error'}, 400);
+    callback({ message: 'The request was an error' }, 400);
   };
   xhr.send();
 }
@@ -22,13 +22,35 @@ const App = () => {
 
   useEffect(() => {
     loadTweets((response: any[], status) => {
-      if(status === 200){
-        setTweets(response)
+      if (status === 200) {
+        setTweets(response);
       }
-    })
+    });
   }, []);
 
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        {tweets.map((tweet) => {
+          return (
+            <Tweet
+              tweet={tweet}
+              key={tweet.id}
+              className='my-5 py-5 border bg-white text-dark'
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
+const Tweet = (props) => {
+  const { tweet, className } = props;
+  return (
+    <div className={className}>
+      <p>{tweet.content}</p>
+    </div>
+  );
+};
 export default App;
