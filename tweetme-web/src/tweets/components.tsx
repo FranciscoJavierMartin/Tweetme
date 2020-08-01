79dbf3a1b1fc8f8ff4431a8d1ea82dc45be8ae5a
Francisco <javiermartincuenca@gmail.com>
2020-08-01 20:12:00 +0200

    59 - Improved Action btn

--- tweetme-web/src/tweets/components.tsx
+++ tweetme-web/src/tweets/components.tsx
@@ -8,20 +8,36 @@ export const Tweet = (props) => {
   return (
     <div className={className}>
       <p>{tweet.content}</p>
-      <ActionBtn tweet={tweet} action={{ type: 'like' }} />
+      <ActionBtn tweet={tweet} action={{ type: 'like', display: 'Likes' }} />
+      <ActionBtn
+        tweet={tweet}
+        action={{ type: 'unlike', display: 'Unlikes' }}
+      />
+      <ActionBtn
+        tweet={tweet}
+        action={{ type: 'retweet', display: 'Retweet' }}
+      />
     </div>
   );
 };
 
 export const ActionBtn = (props) => {
   const { tweet, className, action } = props;
-  return action.type === 'like' ? (
-    <button className={className}>{tweet.likes} Likes</button>
-  ) : null;
+  const actionDisplay = action.display ? action.display : 'Action';
+  let likes = tweet.likes;
+  const handleClick = (event) => {
+    event.preventDefault();
+    if (action.type === 'like') {
+      console.log(likes + 1);
+      likes = tweet + 1;
+    }
+  };
+  const display =
+    action.type === 'like' ? `${likes} ${actionDisplay}` : actionDisplay;
+  return <button className={className}>{display}</button>;
 };
 
 export const TweetsList = (props) => {
-
   const [tweets, setTweets] = useState<any[]>([]);
 
   useEffect(() => {
